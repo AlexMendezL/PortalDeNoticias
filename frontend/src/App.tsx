c0851e411cd0b2f38782f4ef85afe7e25a4bbec8
Alex Méndez <alex.mendez.lopez@outlook.com>
2025-11-10 00:37:54 -0600

    refactor: correciones de init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,30 @@ import {BrowserRouter, HashRouter} from 'react-router'
 import {ThemeProvider} from './contexts/ThemeContext'
 import Router from './Router'
 import {useAuthStore} from "@/store/useAuthStore.ts";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {Spinner} from "@/components/ui/spinner.tsx";
 
 const AppRouter = import.meta.env.VITE_USE_HASH_ROUTE === 'true' ? HashRouter : BrowserRouter
 
 export default function App() {
+    const [initDone, setInitDone] = useState(false)
     const checkAuth = useAuthStore((state) => state.checkAuth)
-    const loading= useAuthStore((state) => state.loading)
 
     useEffect(() => {
-        const init = async () => {
-            await checkAuth()
-        }
-        init()
+        checkAuth()
+            .catch(() => {
+                console.log("Without session")
+            })
+            .finally(() => setInitDone(true))
     }, [checkAuth])
 
-    if (loading) return <div>loading</div>
-
+    if (!initDone) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <Spinner/>
+            </div>
+        )
+    }
     return (
         <ThemeProvider>
             <AppRouter>
