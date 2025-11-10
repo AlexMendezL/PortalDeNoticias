9017215fff1a6aada62ec8b4ec995fb341f58f4c
Alex Méndez <alex.mendez.lopez@outlook.com>
2025-11-09 21:56:14 -0600

    feat: escoder opciones en HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,16 @@
 import {Button} from "@/components/ui/button.tsx";
 import {Link} from "react-router-dom";
+import {useAuthStore} from "@/store/useAuthStore.ts";
 
 export default function HomePage() {
+    const isAuth = useAuthStore((state) => state.auth)
     return (
         <>
             <div className="flex flex-col gap-3 my-auto mx-auto">
 
-                <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold"> Todo lo que necesitas saber, en un solo sitio</h1>
+                <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
+                    Todo lo que necesitas saber, en un solo sitio
+                </h1>
                 <p className="text-lg md:text-xl lg:text-2xl">
                     Explora artículos, blogs y reportes que te mantienen informado y te ayudan a comprender el panorama
                     completo
@@ -14,14 +18,16 @@ export default function HomePage() {
                 <div className="flex items-start gap-2">
                     <Button size="lg" asChild>
                         <Link to="auth/login">
-                                Ver lo más recientes
+                            Ver lo más recientes
                         </Link>
                     </Button>
-                    <Button size="lg" variant="outline" asChild>
-                        <Link to="auth/signup">
-                            Crear cuenta
-                        </Link>
-                    </Button>
+                    {!isAuth &&
+                        <Button size="lg" variant="outline" asChild>
+                            <Link to="auth/signup">
+                                Crear cuenta
+                            </Link>
+                        </Button>
+                    }
                 </div>
             </div>
         </>
