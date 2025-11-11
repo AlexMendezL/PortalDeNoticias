436955f0b8e517d6ac614c8f4c5e0dc9f86bb47a
Alex Méndez <alex.mendez.lopez@outlook.com>
2025-11-10 21:44:47 -0600

    refactor: refactorizacion de endpoints news

--- backend/controller/news_controller.py
+++ backend/controller/news_controller.py
@@ -1,3 +1,17 @@
+from fastapi import HTTPException
+import requests
+
+API_NEWS = "https://api.spaceflightnewsapi.net/v4"
+
 
 class NewsController:
-    pass
+    @staticmethod
+    def get_articles(resource):
+        try:
+            response = requests.get(f"{API_NEWS}/{resource}", timeout=15)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            raise HTTPException(status_code=502, detail=f"Error to connect to extern API: {e}")
+
+        data = response.json()
+        return data
--- backend/routes/news_router.py
+++ backend/routes/news_router.py
@@ -1,16 +1,12 @@
 from fastapi import APIRouter, HTTPException, Depends
-import requests
 
 from middleware.auth_middleware import get_current_user
+
+from controller.news_controller import NewsController
 
 router = APIRouter(
     dependencies=[Depends(get_current_user)]
 )
-
-
-def get_api_url(category: str) -> str:
-    """Construye la URL base de la API externa Spaceflight News API para la categoría especificada."""
-    return f"https://api.spaceflightnewsapi.net/v4/{category}"
 
 
 @router.get(
@@ -28,8 +24,8 @@ def ping():
     "/news",
     summary="Obtener todas las noticias",
     description=(
-        "Consulta la API externa [Spaceflight News API](https://api.spaceflightnewsapi.net/) "
-        "para obtener una lista de artículos recientes."
+            "Consulta la API externa [Spaceflight News API](https://api.spaceflightnewsapi.net/) "
+            "para obtener una lista de artículos recientes."
     ),
     tags=["News"]
 )
@@ -40,15 +36,7 @@ def news():
     Returns:
         Un objeto JSON con los artículos más recientes.
     """
-    url = get_api_url("articles")
-    try:
-        response = requests.get(url, timeout=15)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(status_code=502, detail=f"Error to connect to extern API: {e}")
-
-    data = response.json()
-    return data
+    return NewsController.get_articles("articles")
 
 
 @router.get(
@@ -79,15 +67,7 @@ def detail(category: str, id: int):
     Returns:
         Un objeto JSON con la información detallada de la noticia.
     """
-    url = f"{get_api_url(category)}/{id}"
-    try:
-        response = requests.get(url, timeout=15)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(status_code=502, detail=f"Error to connect to extern API: {e}")
-
-    data = response.json()
-    return data
+    return NewsController.get_articles(f"{category}/{id}")
 
 
 @router.get(
@@ -107,16 +87,7 @@ def related_news(category: str, news_site: str):
     Returns:
         Noticias publicadas por el mismo sitio en la categoría indicada.
     """
-    url = f"{get_api_url(category)}/?news_site={news_site}"
-    print(url)
-    try:
-        response = requests.get(url, timeout=15)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(status_code=502, detail=f"Error to connect to extern API: {e}")
-
-    data = response.json()
-    return data
+    return NewsController.get_articles(f"{category}/?news_site={news_site}")
 
 
 @router.get(
@@ -135,12 +106,4 @@ def news_by_category(category: str | int):
     Returns:
         Lista de noticias pertenecientes a la categoría solicitada.
     """
-    url = get_api_url(category)
-    try:
-        response = requests.get(url, timeout=15)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(status_code=502, detail=f"Error to connect to extern API: {e}")
-
-    data = response.json()
-    return data
+    return NewsController.get_articles(f"{category}")
